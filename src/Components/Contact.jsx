038855b8f5f5597525e543ace9cc5574d90c5539
Imran Shahid <imranshahid111@gmail.com>
2024-08-23 22:37:23 +0500

    fetch

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Contact() {
   return (
     <>
-    <section class="bg-[#181824] dark:bg-gray-900">
+    <section class="bg-[#181824] ">
   <div class="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
       <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-center text-[#7562E0] dark:text-white">Contact Us</h2>
       <p class="mb-8 lg:mb-16 font-light text-center text-gray-300 dark:text-gray-400 sm:text-xl">Got a technical issue? Want to send feedback about a beta feature? Need details about our Business plan? Let us know.</p>
